581ae23fba55b94937ad5c46c84e3a7455c49822
Giovanne Colli <cgiovanne45@gmail.com>
2026-03-07 19:09:33 -0300

    add regex on register user

--- app/schemas/register-user.ts
+++ app/schemas/register-user.ts
@@ -5,7 +5,8 @@ export const registerUser = z.object({
         .email({ message: 'O e-mail está invalido' }),
     password: z
         .string({ message: 'A senha precisa ser definida' })
-        .nonempty({ message: 'A senha não pode estar vazia' }),
+        .nonempty({ message: 'A senha não pode estar vazia' })
+        .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, { message: 'Senha preciso ter no mínimo oito caracteres, pelo menos uma letra maiúscula, uma letra minúscula, um número e um caractere especial.' }),
     confirmPassword: z
         .string({ message: 'A confirmação da senha precisa ser definida' })
         .nonempty({ message: 'A confirmação da senha não pode estar vazia' })
